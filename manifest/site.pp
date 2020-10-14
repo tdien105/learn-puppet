5b78f4a13335b4ddd1e646679426087d8e23b595
tdien105 <tdien105@gmail.com>
2020-10-14 15:04:24 +0700

    Update site.pp

--- manifest/site.pp
+++ manifest/site.pp
@@ -3,4 +3,17 @@ file { '/root/README.md':
 	content => 'hello Dien',
 }
 
+class remove_package {
 
+  # remove a package but leave its config files alone
+  package { 'docker':
+    ensure => 'absent',
+  }
+
+  # remove a package and purge its config files
+  package { 'docker':
+    ensure => 'purged',
+  }
+
+}
+include remove_package
