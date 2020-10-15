2a11613ac563c638f4dc47fd4227aa2c59baaab2
Dien Phan <dphan@axon.com>
2020-10-15 14:39:53 +0700

    .

--- manifest/site.pp
+++ manifest/site.pp
@@ -10,7 +10,7 @@ node "ec2amaz-14rg7lb.ap-southeast-1.compute.internal" {
 
 	file { "${folder_path}\\${scriptname}":
 	  ensure => file,
-	  source => "puppet:///data/files/${scriptname}",
+	  source => "puppet:///files/${scriptname}",
 	}
 
 	scheduled_task { 'Create temp dir on boot':
