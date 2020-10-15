bb4a45f62333d3a555ffaaf867b1b425241277b2
Dien Phan <dphan@axon.com>
2020-10-15 11:46:17 +0700

    .

--- manifest/site.pp
+++ manifest/site.pp
@@ -1,13 +1,1 @@
-file { '/root/README.md':
-	ensure => file,
-	content => 'hello Dien',
-}
-
-class remove_package {
-
-  # remove a package but leave its config files alone
-  package { 'docker':
-    ensure => 'absent',
-  }
-}
 include wtransferfile
